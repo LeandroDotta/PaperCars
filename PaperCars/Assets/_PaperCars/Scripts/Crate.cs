6f42d90ca5239d250c76cf0ea3fa0cee69b844c4
LeandroDotta <leandro.dotta@yahoo.com.br>
2018-12-30 23:32:57 -0200

    Improved the Crate obstacle prefab
    Added sound and particle effect when it breaks.
    
    Also:
    - Added a Sorting Layer "Player" for the player object;
    - Changed the name of "Platforms" sorting layer to "Environment";
    
    Refactoring:
    - Moved the car sprites file to the folder Assets/Sprites/Vehicles;
    - Changed the "Obstacle" script name to "Crate";

---
+++ PaperCars/Assets/_PaperCars/Scripts/Crate.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour
+{
+    [SerializeField] private bool breakable = true;
+    [SerializeField] private float forceToBreak;
+
+    [Header("Components")]
+    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private Collider2D coll;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private ParticleSystem particle;
+
+    private void Reset() 
+    {
+        breakable = true;
+        forceToBreak = 25;
+
+        sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<Collider2D>();
+        audioSource = GetComponent<AudioSource>();
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    protected void Break()
+    {
+        sprite.enabled = false;
+        coll.enabled = false;
+
+        audioSource.Play();
+        particle.Play();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!breakable)
+            return;
+
+        float impactForce = other.relativeVelocity.magnitude;
+
+        Debug.Log("On Crate Collision: " + impactForce);
+
+        if(impactForce >= forceToBreak)
+        {
+            Break();
+        }
+    }
+
+    private void OnParticleSystemStopped()
+    {
+        Debug.LogWarning("OnParticleSystemStopped");
+        Destroy(gameObject);
+    }
+}
